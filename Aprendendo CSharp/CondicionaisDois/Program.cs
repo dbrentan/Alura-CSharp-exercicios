35e90ba13fd91900d8a169811998ac2333678959
dbrentan <danielbrentan@gmail.com>
2022-02-04 15:47:44 -0300

    Aprendendo CSharp curso Alura

---
+++ Aprendendo CSharp/CondicionaisDois/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace CondicionaisDois
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 8 - Condicionais 2");
+
+            int idadeJoao = 18;
+            // int quantidadePessoas = 2;
+
+            bool acompanhado = true;
+
+            if (idadeJoao >= 18 && acompanhado == true)
+            {
+                Console.WriteLine("João pode entrar");
+            }
+            else
+            {   
+                Console.WriteLine("Não pode entrar");
+    
+            }
+
+            int idadePedro = 16;
+           
+            bool acompanhadoPedro = true;
+
+            if (idadePedro >= 18 || acompanhadoPedro == true)
+            {
+                Console.WriteLine("Pedro pode entrar");
+            }
+            else
+            {
+                Console.WriteLine("Pedro Não pode entrar");
+            }
+
+        }
+    }
+}
