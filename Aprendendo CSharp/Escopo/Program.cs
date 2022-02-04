ad8d3ed93d852fd6b68b52415adda23314f07860
dbrentan <danielbrentan@gmail.com>
2022-02-04 17:21:25 -0300

    Aprendendo CSharp curso Alura

---
+++ Aprendendo CSharp/Escopo/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Escopo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 9 - Escopo");
+
+            int idadeJoao = 18;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == false)
+            {
+                mensagemAdicional = "João está Acompanhado";
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado";
+            }
+
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João pode entrar");
+                 Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {   
+                Console.WriteLine("Não pode entrar");
+                Console.WriteLine(mensagemAdicional);
+    
+            }
+
+        }
+    }
+}
