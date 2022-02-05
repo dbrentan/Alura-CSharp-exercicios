9319f1f03360b14ce788056d2539d11497c733a8
dbrentan <danielbrentan@gmail.com>
2022-02-05 18:32:00 -0300

    Aprendendo CSharp curso Alura

---
+++ Aprendendo CSharp/MultiploDeTres/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MultiploDeTres
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Os Múltiplos de 3 são:");
+
+            for (int numero = 1; numero <= 100; numero++)
+            {
+                if (numero % 3 == 0)
+                {
+                    Console.WriteLine(numero);
+                }
+            }
+        }
+    }
+}
