83aad9c96c4f3e62a537df60dbf92b7715ffe41b
dbrentan <danielbrentan@gmail.com>
2022-02-05 16:14:12 -0300

    Aprendendo CSharp curso Alura

---
+++ Aprendendo CSharp/CalculaPoupancaDois/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CalculaPoupancaDois
+{
+    public class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Executando projeto 11: Usando o laço For");
+
+            double valorInvestido = 1000;
+
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;  //Juros por mês
+
+                if (contadorMes == 1)
+                {
+                    Console.WriteLine($"Após {contadorMes} mês, você terá R${valorInvestido.ToString("0.00")}");
+                }
+                else
+                {
+                    Console.WriteLine($"Após {contadorMes} meses, você terá R${valorInvestido.ToString("0.00")}");
+                }
+            }
+
+        }
+    }
+}
