37d1a992334d2d4fecc62d3944f658bf05f54c9c
dbrentan <danielbrentan@gmail.com>
2022-02-08 12:06:12 -0300

    Aprendendo CSharp curso Alura

---
+++ Aprendendo CSharp POO/ByteBankParteDois/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ByteBankParteDois
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine($"Seu saldo atual é de: {contaDoBruno.saldo}");
+
+            bool resultado = contaDoBruno.Sacar(50);
+
+            Console.WriteLine($"Seu saldo atual é de: {contaDoBruno.saldo}");
+            Console.WriteLine(resultado);
+
+        }
+    }
+}
